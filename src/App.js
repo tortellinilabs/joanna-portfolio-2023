4e8af67c2fd10a0c3067084640993b98c19ed9d4
hactuallybenji <mrbenamiller@gmail.com>
2023-09-24 11:37:23 -0700

    Adds autoplaying carousel component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import CarouselDefault from './components/CarouselDefault';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/home" element={ <Header /> } />
+      <Route path="/carousel" element={ <CarouselDefault /> } />
     </Routes>
     </>
   );
