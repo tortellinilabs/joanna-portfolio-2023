e94d2394b27c1561ffdf2687358e5e866b2d954a
hactuallybenji <mrbenamiller@gmail.com>
2023-09-24 14:03:37 -0700

    Adds image descriptions, and removes extraneous imports

--- src/components/CarouselDefault.jsx
+++ src/components/CarouselDefault.jsx
@@ -1,4 +1,4 @@
-import { Carousel, Typography, Button } from "@material-tailwind/react";
+import { Carousel } from "@material-tailwind/react";
 
 const CarouselDefault = () => {
   return (
@@ -28,17 +28,17 @@ const CarouselDefault = () => {
         >
           <img
             src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-            alt="image 1"
+            alt="Aerial view of a pristine lake meeting a tree-covered shoreline"
             className="h-full w-full object-cover"
           />
           <img
             src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-            alt="image 2"
+            alt="Ground view of a pristine lake in a valley surrounded by rock-faced mountains"
             className="h-full w-full object-cover"
           />
           <img
             src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-            alt="image 3"
+            alt="Aerial view of land with two small structures jutting out into the ocean"
             className="h-full w-full object-cover"
           />
         </Carousel>
