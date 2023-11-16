1cd6abb9bb0b88c351c9d56d79dad0b04086bf90
hactuallybenji <mrbenamiller@gmail.com>
2023-11-16 06:26:05 -0800

    Indents to make code cleaner

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import ProgressiveImage from './components/ProgressiveImage';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import ProgressiveImage from "./components/ProgressiveImage";
+import CountUp from "./components/CountUp";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/home" element={ <Header /> } />
-      <Route path="/customimage" element={
-        <ProgressiveImage
-          src={require('./images/Tortellini.png')}
-          placeholderSrc="./images/Small_Tortellini.png"
-          width="700"
-          height="700"
-        /> } />
-    </Routes>
+      <Routes>
+        <Route path="/home" element={<Header />} />
+        <Route
+          path="/customimage"
+          element={
+            <ProgressiveImage
+              src={require("./images/Tortellini.png")}
+              placeholderSrc="./images/Small_Tortellini.png"
+              width="700"
+              height="700"
+            />
+          }
+        />
+        <Route path="/countup" element={<CountUp start={0} end={2000} />} />
+      </Routes>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import Button from './Button';
+import Button from "./Button";
 
 const Header = () => {
   return (
     <>
-    <nav className="flex justify-center p-5">
-      <Button text="Social Media" />
-      <Button text="Email Marketing" />
-      <Button text="About Me" />
-    </nav>
+      <nav className="flex justify-center p-5">
+        <Button text="Social Media" />
+        <Button text="Email Marketing" />
+        <Button text="About Me" />
+      </nav>
     </>
   );
-}
+};
 
 export default Header;
